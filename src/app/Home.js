d91f9cd95c0674bc0bb58c620919ae70ff762be3
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2023-06-03 17:52:26 +0300

    Text about work in progress

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,9 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { useTranslation } from 'react-i18next';
+import { FaSpinner } from 'react-icons/fa';
 
 import { LayoutContainer } from '../layouts/Default';
 import Breadcrumb from '../layouts/Breadcrumb';
@@ -18,7 +19,7 @@ function NameChecker() {
       return (
         <Row>
           <Col>
-            <Card className="mb-4">
+            <Card className="mb-4 p-4">
               <Card.Body>
                 <h5>{t('Welcome!')}</h5>
                 <p>
@@ -51,17 +52,33 @@ function MyBreadcrumb() {
   );
 }
 
+function WorkInProgress() {
+  return (
+    <div className="d-flex justify-content-center align-items-center p-4 h-100">
+      <div className="text-center">
+        <h3 className="mb-4">
+          <FaSpinner />
+        </h3>
+        <h5 className="mb-4">Palvelua vielä kehitetään</h5>
+        <p>
+          Kiitos kun rekisteröidyt palveluun! Ole hyvä ja odota, kunnes saat
+          lisätietoa siitä, milloin pääset anomaan vuoroja. Kärsivällisyys
+          palkitaan!
+        </p>
+      </div>
+    </div>
+  );
+}
+
 export default function Home() {
-  const { t } = useTranslation();
   return (
     <LayoutContainer breadcrumb={<MyBreadcrumb />}>
       <NameChecker />
       <Row>
         <Col>
-          <Card className="mb-4">
+          <Card className="mb-4" style={{ minHeight: '20em' }}>
             <Card.Body>
-              <h1>Home</h1>
-              <Link to="/projects">{t('Projects')}</Link>
+              <WorkInProgress />
             </Card.Body>
           </Card>
         </Col>
