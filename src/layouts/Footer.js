4181608d18081704e1d02a32f9c7412c9eeb5b97
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2023-06-03 17:51:49 +0300

    Copyright text in footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,15 +1,24 @@
+import { useTranslation } from 'react-i18next';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 export default function Footer() {
+  const { t } = useTranslation();
   return (
-    <footer className="footer pt-3">
+    <footer className="footer">
       <Container fluid>
         <Row className="align-items-center justify-content-lg-between">
           <Col className="mb-lg-0 mb-4">
-            <div className="copyright text-center text-sm text-muted text-lg-start">
-              Cart Scheduler
+            <div className="copyright text-center text-xs text-muted text-lg-start">
+              {process.env.REACT_APP_TITLE}{' '}
+              {t(
+                'is a service maintained by private individuals and is based on open source project',
+              )}{' '}
+              <a href="https://github.com/Cart-Scheduler/Cart-Scheduler">
+                Cart&nbsp;Scheduler
+              </a>
+              .
             </div>
           </Col>
         </Row>
