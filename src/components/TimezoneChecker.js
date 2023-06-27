c71db345b06dd01c1dc5e01413e85b4f9a34ea17
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2023-06-27 23:57:13 +0300

    TimezoneChecker: small changes

--- src/components/TimezoneChecker.js
+++ src/components/TimezoneChecker.js
@@ -1,29 +1,54 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import { FaExclamationCircle } from 'react-icons/fa';
 import { useTranslation } from 'react-i18next';
 
 function TimezoneChecker() {
   const { t } = useTranslation();
   const defaultTimezone = process.env.REACT_APP_DEFAULT_TIMEZONE;
+  if (
+    !defaultTimezone ||
+    !Intl ||
+    !Intl.DateTimeFormat ||
+    !Intl.DateTimeFormat().resolvedOptions ||
+    !Intl.DateTimeFormat().resolvedOptions().timeZone
+  ) {
+    return null;
+  }
   const currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-  console.log('Default timezone: ', defaultTimezone);
-  console.log('Current timezone: ', currentTimezone);
 
   // Current tz default --> null
   if (currentTimezone === defaultTimezone) {
     return null;
   }
 
-  // If in wrong timzone
   return (
-    <Card className="mb-4">
-      <Card.Body>
-        {t(
-          'You have set the wrong time zone, please check your time zone in the device settings',
-        )}
-      </Card.Body>
-    </Card>
+    <Alert variant="danger" className="text-white">
+      <div className="d-flex align-items-center">
+        <div className="ps-2 pe-3 d-none d-md-block">
+          <FaExclamationCircle size={28} />
+        </div>
+        <div>
+          <p className="mb-2">
+            {t(
+              'Shifts and times can be incorrect, because the timezone of your device and the website are different:',
+            )}
+          </p>
+          <ul className="mb-1">
+            <li>
+              <strong>{t('Timezone in your device:')}</strong> {currentTimezone}
+            </li>
+            <li>
+              <strong>{t('Website timezone:')}</strong> {defaultTimezone}
+            </li>
+          </ul>
+          <p className="mb-1">
+            {t(
+              'We recommend that you use automatic timezone setting in your device settings.',
+            )}
+          </p>
+        </div>
+      </div>
+    </Alert>
   );
 }
 
