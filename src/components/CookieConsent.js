f2068e68eea2aea6037fc8d83506f636fad99f47
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2023-07-01 12:26:10 +0300

    CookieConsent: use auto height

--- src/components/CookieConsent.js
+++ src/components/CookieConsent.js
@@ -5,22 +5,9 @@ import Container from 'react-bootstrap/Container';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-export default function CookieConsent(props) {
+export default function CookieConsent() {
   const [show, setShow] = useState(false);
   const { t } = useTranslation();
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  const handleResize = (e) => {
-    setWindowWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
 
   useEffect(() => {
     const cookieConsent = localStorage.getItem('cookieConsent');
@@ -34,24 +21,20 @@ export default function CookieConsent(props) {
     setShow(false);
   };
 
-  const offcanvasStyle = windowWidth <= 768 ? { height: '36%' } : {};
-
   return (
     <Offcanvas
       show={show}
-      onHide={() => {}}
       backdrop="static"
       placement="bottom"
-      style={offcanvasStyle}
-      {...props}
+      className="h-auto overflow-auto"
     >
-      <Container className="mx-auto">
-        <Offcanvas.Header closeButton={false} className="mx-auto pb-2 pt-4">
-          <Offcanvas.Title className="mx-1 mt-1">
+      <Container>
+        <Offcanvas.Header closeButton={false} className="pb-2 pt-4">
+          <Offcanvas.Title className="mt-1">
             {t('Cookie Consent')}
           </Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body className="mx-1">
+        <Offcanvas.Body>
           <div className="mb-4">
             {t(
               'To provide you with the best possible experience, we use cookies and similar technologies. By continuing to use our site, you agree to the use of cookies as described in our ',
