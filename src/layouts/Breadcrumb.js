00139755b1d6269957d0b4298d02a7cffc369c01
xMal0c <jonu.owizyc@gmail.com>
2023-07-03 11:15:42 +0300

    Breadcrumb themes added

--- src/layouts/Breadcrumb.js
+++ src/layouts/Breadcrumb.js
@@ -3,25 +3,20 @@ import { Link } from 'react-router-dom';
 
 function Item({ to, children }) {
   return (
-    <li className="breadcrumb-item text-sm text-white">
-      {to ? (
-        <Link to={to} className="text-white">
-          {children}
-        </Link>
-      ) : (
-        children
-      )}
+    <li className="breadcrumb-item text-sm">
+      {to ? <Link to={to}>{children}</Link> : children}
     </li>
   );
 }
 
-function Breadcrumb({ children, title }) {
+function Breadcrumb({ children, title, theme }) {
+  const mainClass = `breadcrumb-theme-${theme === 'light' ? 'light' : 'dark'}`;
   return (
-    <nav aria-label="breadcrumb">
+    <nav aria-label="breadcrumb" className={mainClass}>
       <ol className="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
         {children}
       </ol>
-      <h6 className="font-weight-bolder text-white mb-0">{title}</h6>
+      <h6 className="font-weight-bolder mb-0">{title}</h6>
     </nav>
   );
 }
