a3ae0793b225cae94954c78aa8da1e50158a544e
TMankinen95 <topi.mankinen@gmail.com>
2025-11-18 09:46:11 +0200

    Added routing to signout, profile, projects etc. And private layout

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,25 @@ export const DEFAULT_PATH = '/';
 
 export default [
   layout('./layouts/SignIn.jsx', [
-    index('./routes/seppo.jsx'),
     ...prefix('signin', [
-      //index('./routes/signin/page.jsx'),
       index('./routes/signin/page.jsx'),
       route('link', './routes/signin/Link.jsx'),
     ]),
+    route('signout', './routes/SignOut.jsx'),
+  ]),
+
+  layout('./layouts/Private.jsx', [
+    index('./routes/seppo.jsx'),
+    route('profile', './routes/profile/index.jsx'),
+    route('projects', './routes/projects/index.jsx'),
+    route(
+      'projects/:projectId',
+      './routes/projects/project.$projectId/index.jsx',
+    ),
+    route(
+      'projects/:projectId/admin/members',
+      './routes/projects/project.$projectId/admin/members/index.jsx',
+    ),
+    route('join/:projectId', './routes/join.$projectId/index.jsx'),
   ]),
 ];
