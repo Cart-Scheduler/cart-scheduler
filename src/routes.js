688e5d19c5616a97409915c869fd8a537cc7f78c
TMankinen95 <topi.mankinen@gmail.com>
2025-12-02 11:27:30 +0200

    ./routes back to ./app

--- src/routes.js
+++ src/routes.js
@@ -5,34 +5,29 @@ export const DEFAULT_PATH = '/';
 export default [
   layout('./layouts/SignIn.jsx', [
     ...prefix('signin', [
-      index('./routes/signin/page.jsx'),
-      route('link', './routes/signin/Link.jsx'),
+      index('./app/signin/page.jsx'),
+      route('link', './app/signin/Link.jsx'),
     ]),
-    ...prefix('signup', [
-      route('password', './routes/signup/password/page.jsx'),
-    ]),
-    route('signout', './routes/SignOut.jsx'),
+    ...prefix('signup', [route('password', './app/signup/password/page.jsx')]),
+    route('signout', './app/SignOut.jsx'),
   ]),
 
-  index('./routes/Home.jsx'),
+  index('./app/Home.jsx'),
 
-  route('profile', './routes/profile/page.jsx'),
+  route('profile', './app/profile/page.jsx'),
 
   ...prefix('projects', [
-    index('./routes/projects/page.jsx'),
-    route('new', './routes/projects/new/page.jsx'),
+    index('./app/projects/page.jsx'),
+    route('new', './app/projects/new/page.jsx'),
     ...prefix(':projectId', [
-      index('./routes/projects/project/page.jsx'),
-      route('admin', './routes/projects/project/admin/page.jsx'),
-      route(
-        'admin/members',
-        './routes/projects/project/admin/members/page.jsx',
-      ),
+      index('./app/projects/project/page.jsx'),
+      route('admin', './app/projects/project/admin/page.jsx'),
+      route('admin/members', './app/projects/project/admin/members/page.jsx'),
     ]),
   ]),
 
-  route('join/:projectId', './routes/join/page.jsx'),
+  route('join/:projectId', './app/join/page.jsx'),
 
-  route('privacy-policy', './routes/PrivacyPolicy.jsx'),
-  route('terms-of-use', './routes/TermsOfUse.jsx'),
+  route('privacy-policy', './app/PrivacyPolicy.jsx'),
+  route('terms-of-use', './app/TermsOfUse.jsx'),
 ];
