2a0a615aeb2df4e9c24bbf01d463e767df6cb7c2
TMankinen95 <topi.mankinen@gmail.com>
2025-12-15 15:04:22 +0200

    Fixed font by adiding it to root.jsx file

--- src/root.jsx
+++ src/root.jsx
@@ -19,6 +19,10 @@ export function Layout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
