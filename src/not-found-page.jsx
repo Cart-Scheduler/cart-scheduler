89be1364321a8cdd16b70b6c307223f125395375
TMankinen95 <topi.mankinen@gmail.com>
2025-12-19 13:47:13 +0200

    Added translation to 404 page

--- src/not-found-page.jsx
+++ src/not-found-page.jsx
@@ -1,6 +1,8 @@
 import { Container, Card } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 export function NotFoundPage() {
+  const { t } = useTranslation();
   return (
     <Container className="py-2 py-md-5 px-3">
       <div className="text-center py-5">
@@ -10,7 +12,9 @@ export function NotFoundPage() {
         >
           <Card.Body className="p-3 p-md-5">
             <h2 className="display-4 mb-4 text-primary fw-bold">404</h2>
-            <h4 className="text-dark mb-4 fw-normal px-2">Sivua ei löytynyt</h4>
+            <h4 className="text-dark mb-4 fw-normal px-2">
+              {t('Page not found')}
+            </h4>
             <p
               className="mb-5 text-muted px-2 px-md-4"
               style={{
@@ -18,16 +22,17 @@ export function NotFoundPage() {
                 lineHeight: '1.6',
               }}
             >
-              Valitettavasti etsimääsi osoitetta ei ole olemassa.
-              <span className="d-block d-md-inline mt-2 mt-md-0">
-                Tarkista osoite ja yritä uudestaan.
+              {t('The requested page could not be found.')}
+              <span className="d-block d-md-inline mt-2 mt-md-0 ms-md-1">
+                {t('Please check the URL and try again.')}
               </span>
             </p>
+
             <a
               href="/"
               className="btn btn-primary btn-lg px-5 shadow w-100 w-md-auto"
             >
-              Siirry etusivulle
+              {t('Return Home')}
             </a>
           </Card.Body>
         </Card>
